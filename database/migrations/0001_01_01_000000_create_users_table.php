47563855a1f9dca5739ba5dc1726e179d5ceeebd
Rivaldisky73 <rivaldiramadani73@gmail.com>
2025-03-24 18:00:44 +0700

    menambah 4 database migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nama', 255);
+            $table->string('alamat', 255);
+            $table->string('no_hp', 50);
+            $table->string('email', 50)->unique();
+            $table->string('role', 50);
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
